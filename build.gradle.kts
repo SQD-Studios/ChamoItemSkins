adcac4c15de05621ea281a14bf4b96845ac2a527
Chamogelastos <stavros.ssta@gmail.com>
2026-09-12 14:35:07 +0300

    Bump version to 1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,4 @@ dependencies {
     //dokka(project(":plugin:"))
 
     implementation(project(":plugin"))
-    implementation("org.bstats:bstats-bukkit:3.2.1")
-}
-
-tasks {
-    shadowJar {
-        dependsOn(":plugin:shadowJar")
-
-        configurations = project.configurations.runtimeClasspath.map { setOf(it) }
-        relocate("org.bstats", project.group.toString())
-    }
 }
