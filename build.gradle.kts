f05193206ef7f1a4489f008ad9405c0c50a3a16d
Chamogelastos <stavros.ssta@gmail.com>
2026-09-12 14:45:29 +0300

    Change the workflows for the sixth time this week (prob)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
-    id("com.gradleup.shadow") version "9.6.1"
     id("shared")
 }
 
 dependencies {
     //dokka(project(":api:"))
     //dokka(project(":plugin:"))
-
-    implementation(project(":plugin"))
 }
